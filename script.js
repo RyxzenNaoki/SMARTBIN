9c33158ed1f52da33de5d957a84c9a8c96cdf6d3
RyxzenNaoki <sabrinahapsari21@gmail.com>
2025-04-21 12:24:23 +0700

    add firebase-admin dependency

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
-import { getFirestore, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getFirestore, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCh-323pn0rAagXRgXwslRi2LoFq6xHKNs",
